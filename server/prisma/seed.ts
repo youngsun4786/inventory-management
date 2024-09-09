df60cdf86b20a302b34189f36ca8b4bf083cbf7a
Youngsun Yoo <youngsun123q@gmail.com>
2024-09-09 01:56:43 -0600

    configure tsconfig file and add seed.ts

---
+++ server/prisma/seed.ts
@@ -0,0 +1,67 @@
+import { PrismaClient } from "@prisma/client";
+import fs from "fs";
+import path from "path";
+
+const prisma = new PrismaClient();
+
+const deleteAllData = async (orderedFileNames: string[]) => {
+  const modelNames = orderedFileNames.map((fileName) => {
+    const modelName = path.basename(fileName, path.extname(fileName));
+    return modelName.charAt(0).toUpperCase() + modelName.slice(1);
+  });
+
+  for (const modelName of modelNames) {
+    const model = prisma[modelName as keyof typeof prisma];
+    if (model) {
+      await model.deleteMany({});
+      console.log(`Cleared data from ${modelName}`);
+    } else {
+      console.error(
+        `Model ${modelName} not found. Please ensure the model name is correctly specified.`
+      );
+    }
+  }
+};
+
+async function main() {
+  const dataDirectory = path.join(__dirname, "seed");
+
+  const orderedFileNames = [
+    "products.json",
+    "expenseSummary.json",
+    "sales.json",
+    "salesSummary.json",
+    "purchases.json",
+    "purchaseSummary.json",
+    "users.json",
+    "expenses.json",
+    "expenseByCategory.json",
+  ];
+
+  await deleteAllData(orderedFileNames);
+
+  for (const fileName of orderedFileNames) {
+    const filePath = path.join(dataDirectory, fileName);
+    const jsonData = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+    const modelName = path.basename(fileName, path.extname(fileName));
+    const model = prisma[modelName as keyof typeof prisma];
+
+    if (!model) {
+      console.error(`No Prisma model matches the file name ${fileName}`);
+    }
+
+    for (const data of jsonData) {
+      await model.create({ data });
+    }
+
+    console.log(`Seeded ${modelName} with data from ${fileName}`);
+  }
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
