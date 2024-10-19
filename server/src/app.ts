eb4d044f2a0e4d5137bb901a83a8d785a127975f
Youngsun Yoo <youngsun123q@gmail.com>
2024-10-19 16:09:42 -0600

    add pm2 file

--- server/src/app.ts
+++ server/src/app.ts
@@ -20,7 +20,7 @@ app.use(cors());
 configureRoutes(app);
 
 /* SERVER */
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
 });
